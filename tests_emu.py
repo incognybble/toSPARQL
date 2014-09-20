2dab41bc106586e31e15fde223e07eb8a9113e70
incognybble <incognybble@users.noreply.github.com>
2014-09-20 17:56:05 +1000

    Clean up of EmuQL unit tests and updating unit test for "not" concept

--- tests_emu.py
+++ tests_emu.py
@@ -1,50 +1,11 @@
 import rdflib
 import os
 import unittest
-import re
 import pyparsing
 from datetime import datetime
 
 import emu_parser
 import conversion_tools
-
-
-def get_files():
-    path = "" # needs data!
-    total_files = []
-    
-    files = os.listdir(path)
-
-    for f in files:
-        loc = os.path.join(path, f)
-        if not os.path.isdir(loc):
-            ext = os.path.splitext(loc)[1]
-            if ext == ".rdf":
-                total_files.append(loc)
-
-
-    return total_files
-
-def get_graph():
-    g = rdflib.graph.Graph()
-    files = get_files()
-
-    total = float(len(files))
-    counter = float(1)
-
-    print total
-    
-    for f in files:
-        if counter%500==0:
-            print str((counter/total)*100) + "% - " + f
-        g.parse(f, format="n3")
-        counter += 1
-
-    return g
-
-
-def clean_whitespace(text):
-    return re.sub("\s+", " ", text)
 
 
 class TestEmuConverter(unittest.TestCase):
@@ -54,9 +15,7 @@ class TestEmuConverter(unittest.TestCase):
 
     def tearDown(self):
         self.end = datetime.now()
-        print "Start: " + str(self.start)
-        print "End: " + str(self.end)
-        print "Timing: " + str(self.end-self.start)
+        print self.end-self.start
 
     def test_EmuParser(self):
         query = "maus:orthography='time'"
@@ -82,8 +41,8 @@ class TestEmuConverter(unittest.TestCase):
                 ?var1 dada:label 'time'.
         }"""
 
-        q = clean_whitespace(q)
-        q_expected = clean_whitespace(q_expected)
+        q = conversion_tools.clean_whitespace(q)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
         self.assertEqual(q, q_expected)
 
 
@@ -120,8 +79,8 @@ class TestEmuConverter(unittest.TestCase):
                 \?(?P=var1) dada:label 't'\..*?
         }"""
         
-        q = clean_whitespace(sparql)
-        q_expected = clean_whitespace(q_expected)
+        q = conversion_tools.clean_whitespace(sparql)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
         
         self.assertRegexpMatches(q, q_expected)
 
@@ -158,8 +117,8 @@ class TestEmuConverter(unittest.TestCase):
                 \?(?P=var1) dada:label 'time'\..*?
         }"""
         
-        q = clean_whitespace(sparql)
-        q_expected = clean_whitespace(q_expected)
+        q = conversion_tools.clean_whitespace(sparql)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
         
         self.assertRegexpMatches(q, q_expected)
         
@@ -205,8 +164,8 @@ class TestEmuConverter(unittest.TestCase):
                 \?(?P=var1) rdf:type dada:Annotation.
         }"""
 
-        q = clean_whitespace(sparql)
-        q_expected = clean_whitespace(q_expected)
+        q = conversion_tools.clean_whitespace(sparql)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
 
         self.assertRegexpMatches(q, q_expected)
         
@@ -275,8 +234,8 @@ class TestEmuConverter(unittest.TestCase):
                 \?(?P=var1) dada:label 't'\..*?
         }"""
         
-        q = clean_whitespace(sparql)
-        q_expected = clean_whitespace(q_expected)
+        q = conversion_tools.clean_whitespace(sparql)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
         
         self.assertRegexpMatches(q, q_expected)
 
@@ -310,15 +269,45 @@ class TestEmuConverter(unittest.TestCase):
             self.assertRegexpMatches(results[0][key]["value"],
                                      "http://ns.ausnc.org.au/corpora/mitcheldelbridge/annotation/\d+")
 
-        # local
 
     def test_EmuSparql9(self):
-        """Testing != : NEEDS UNITTEST"""
+        """Testing !="""
 
         query = "rdf:type=dada:Annotation&maus:phonetic!='t'"
-        q = emu_parser.emuToSparql(query)
         
+        # parser
+        p = emu_parser.parser(query)
+        p_expected_keys = ['connector', 'left', 'right']
+
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        self.assertItemsEqual(p['exp'].keys(), p_expected_keys)
+
+        self.assertEqual(p.exp.connector, "&")
+
+        self.assertEqual(type(p.exp.left), pyparsing.ParseResults)
+        self.assertEqual(p.exp.left.left.text, "rdf:type")
+        self.assertEqual(p.exp.left.right.text, "dada:Annotation")
+        self.assertEqual(p.exp.left.connector, "=")
+        
+        self.assertEqual(p.exp.right.left.text, "maus:phonetic")
+        self.assertEqual(p.exp.right.right.text, "t")
+        self.assertEqual(p.exp.right.connector, "!=")
+
         # sparql
+        sparql = emu_parser.emuToSparql(query)
+        q_expected="""select \?(?P<var1>\w+)
+        where {.*?
+                \?(?P=var1) rdf:type dada:Annotation\..*?
+                \?(?P=var1) dada:type maus:phonetic\..*?
+                FILTER NOT EXISTS {.*?
+                    \?(?P=var1) dada:label 't'\..*?
+                }
+        }"""
+        
+        q = conversion_tools.clean_whitespace(sparql)
+        q_expected = conversion_tools.clean_whitespace(q_expected)
+        
+        self.assertRegexpMatches(q, q_expected)
 
         # pyalveo
         results = conversion_tools.pyalveoQuery(q, limit=True)
@@ -329,9 +318,7 @@ class TestEmuConverter(unittest.TestCase):
             self.assertRegexpMatches(results[0][key]["value"],
                                      "http://ns.ausnc.org.au/corpora/mitcheldelbridge/annotation/\d+")
 
-        # local
 
 
 if __name__ == "__main__":
-    #g = get_graph()
     unittest.main(verbosity=2, exit=False)
