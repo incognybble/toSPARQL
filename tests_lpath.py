6f93948203608875bb07c5720859faee777997e1
incognybble <incognybble@users.noreply.github.com>
2014-09-21 20:27:53 +1000

    Added some lpath+ unit tests

--- tests_lpath.py
+++ tests_lpath.py
@@ -8,50 +8,109 @@ import lpath_parser
 import conversion_tools
 
 
-def get_files():
-    path = "" # needs data!
-    total_files = []
-    
-    files = os.listdir(path)
-
-    for f in files:
-        loc = os.path.join(path, f)
-        if not os.path.isdir(loc):
-            ext = os.path.splitext(loc)[1]
-            if ext == ".rdf":
-                total_files.append(loc)
-
-
-    return total_files
-
-def get_graph():
-    g = rdflib.graph.Graph()
-    files = get_files()
-
-    total = float(len(files))
-    counter = float(1)
-
-    print total
-    
-    for f in files:
-        if counter%500==0:
-            print str((counter/total)*100) + "% - " + f
-        g.parse(f, format="n3")
-        counter += 1
-
-    return g
-
-
-
-def clean_whitespace(text):
-    return re.sub("\s+", " ", text)
-
-
 class TestLPathConverter(unittest.TestCase):
     """LPath+ to SPARQL converter"""
     def setUp(self):
         pass
 
+    def test_LpathParser(self):
+        pass
+
+    def test_LpathToSparql(self):
+        pass
+
+    def test_LpathChild(self):
+        query = "//time/Ae"
+
+        # parser
+        p = lpath_parser.parser(query)
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        
+        self.assertEqual(p.exp.connector, "//")
+        self.assertEqual(p.exp.right.left.text, "time")
+        self.assertEqual(p.exp.right.right.text, "Ae")
+        self.assertEqual(p.exp.right.connector, "/")
+        
+        # sparql
+        q = lpath_parser.lpathToSparql(query)
+
+        #print q
+
+    def test_LpathSequence(self):
+        query = "//t->Ae"
+        
+        # parser
+        p = lpath_parser.parser(query)
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        
+        self.assertEqual(p.exp.connector, "//")
+        self.assertEqual(p.exp.right.left.text, "t")
+        self.assertEqual(p.exp.right.right.text, "Ae")
+        self.assertEqual(p.exp.right.connector, "->")
+        
+        # sparql
+        q = lpath_parser.lpathToSparql(query)
+        
+    def test_LpathPredicate(self):
+        query = "//time[/Ae]"
+
+        # parser
+        p = lpath_parser.parser(query)
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        
+        self.assertEqual(p.exp.connector, "//")
+        self.assertEqual(p.exp.right.text, "time")
+        self.assertEqual(p.exp.right.predicate.right.text, "Ae")
+        self.assertEqual(p.exp.right.predicate.connector, "/")
+        
+        # sparql
+        q = lpath_parser.lpathToSparql(query)
+
+    def test_LpathPredicateNest(self):
+        pass
+
+    def test_LpathAttribute(self):
+        query = "//time[@dada:type=maus:orthography]"
+        
+        # parser
+        p = lpath_parser.parser(query)
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        
+        self.assertEqual(p.exp.connector, "//")
+        self.assertEqual(p.exp.right.text, "time")
+        self.assertEqual(p.exp.right.attr_test.attr, "dada:type")
+        self.assertEqual(p.exp.right.attr_test.attr_val, "maus:orthography")
+        
+        # sparql
+        q = lpath_parser.lpathToSparql(query)
+
+    def test_LpathWildcard(self):
+        query = "//_[/t]"
+        
+        # parser
+        p = lpath_parser.parser(query)
+        self.assertEqual(type(p), pyparsing.ParseResults)
+        
+        self.assertEqual(p.exp.connector, "//")
+        self.assertEqual(p.exp.right.text, "_")
+        self.assertEqual(p.exp.right.predicate.right.text, "t")
+        self.assertEqual(p.exp.right.predicate.connector, "/")
+        
+        # sparql
+        q = lpath_parser.lpathToSparql(query)
+
+    def test_LpathNot(self):
+        pass
+
+    def test_LpathOr(self):
+        pass
+
+    def test_LpathAnd(self):
+        pass
+
+    def test_LpathFunction(self):
+        pass
+
+
 if __name__ == "__main__":
-    #g = get_graph()
     unittest.main(exit=False)
