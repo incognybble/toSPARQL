af7d8c0865072a9da90432224a3db78377e5f81a
incognybble <incognybble@users.noreply.github.com>
2014-09-26 17:52:21 +1000

    Adding tests result set length to query tests

--- tests_queries.py
+++ tests_queries.py
@@ -21,33 +21,42 @@ class TestQueries(unittest.TestCase):
         # EmuQL
         query = "maus:phonetic='t'"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.pyalveoQuery(q, limit=True)
+        results = conversion_tools.pyalveoQuery(q)
         emu_res = results[0]
+        emu_ress = results
         
         # LPath+
         query = "//t[@dada:type=maus:phonetic]"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.pyalveoQuery(q, limit=True)
+        results = conversion_tools.pyalveoQuery(q)
         lpath_res = results[0]
+        lpath_ress = results
 
         self.assertEqual(lpath_res["var1"]["value"], emu_res["var1"]["value"])
+        self.assertEqual(len(emu_ress), len(lpath_ress))
 
     def test_sequence(self):
         """Sequence: Find a 't' followed by a 'Ae', where both are phonetic"""
         # EmuQL
         query = "[maus:phonetic='t'->#maus:phonetic='Ae']"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.pyalveoQuery(q, limit=True)
+        results = conversion_tools.pyalveoQuery(q)
         emu_res = results[0]
+        emu_ress = results
         
         # LPath+
         query = "//t[@dada:type=maus:phonetic]->Ae[@dada:type=maus:phonetic]"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.pyalveoQuery(q, limit=True)
+        results = conversion_tools.pyalveoQuery(q)
         lpath_res = results[0]
+        lpath_ress = results
 
-        #self.assertEqual(lpath_res["var1"]["value"], emu_res["var1"]["value"])
         self.assertEqual(lpath_res["var2"]["value"], emu_res["var2"]["value"])
+        self.assertEqual(len(emu_ress), len(lpath_ress))
 
     def test_dominance(self):
         """Dominance: Find a phonetic 'Ae' dominated by orthography 'time'.
@@ -62,9 +71,11 @@ class TestQueries(unittest.TestCase):
                 ?var1 dada:type maus:phonetic.
                 ?var1 dada:label 'Ae'.
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         
         hand_short_res = results[0]
+        hand_short_ress = results
 
         # sparql indirect
         q="""select ?var0
@@ -84,26 +95,36 @@ class TestQueries(unittest.TestCase):
                 filter (?start0 >= ?start2).
                 filter (?end0 <= ?end2).
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
 
         hand_long_res = results[0]
+        hand_long_ress = results
         
         self.assertEqual(hand_short_res["var1"]["value"], hand_long_res["var0"]["value"])
+        self.assertEqual(len(hand_short_ress), len(hand_long_ress))
 
         # EmuQL
         query = "[maus:orthography='time'^#maus:phonetic='Ae']"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         emu_res = results[0]
+        emu_ress = results
         
         # LPath+
         query = "//time[@dada:type=maus:orthography]/Ae[@dada:type=maus:phonetic]"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         lpath_res = results[0]
+        lpath_ress = results
 
-        #self.assertEqual(lpath_res["var1"]["value"], emu_res["var1"]["value"])
         self.assertEqual(lpath_res["var2"]["value"], emu_res["var2"]["value"])
+        self.assertEqual(len(lpath_ress), len(emu_ress))
+        
+        self.assertEqual(emu_res["var2"]["value"], hand_long_res["var0"]["value"])
+        self.assertEqual(len(emu_ress), len(hand_long_ress))
 
     def test_ancestor(self):
         pass
@@ -134,8 +155,8 @@ class TestQueries(unittest.TestCase):
         }"""
         results = conversion_tools.pyalveoQuery(q, limit=True)
 
-        for result in results:
-            print result["text"]["value"]+ ":" + result["var0"]["value"]
+        #for result in results:
+        #    print result["text"]["value"]+ ":" + result["var0"]["value"]
 
     def test_last2(self):
         """Right-most: Find words which end with phonetic 't'"""
@@ -155,8 +176,8 @@ class TestQueries(unittest.TestCase):
         }"""
         results = conversion_tools.pyalveoQuery(q, limit=True)
 
-        for result in results:
-            print result["var2"]["value"] + ":" + result["text"]["value"]
+        #for result in results:
+        #    print result["var2"]["value"] + ":" + result["text"]["value"]
 
     def test_first(self):
         """Left-most: Find words which start with phonetic 's'"""
@@ -176,8 +197,8 @@ class TestQueries(unittest.TestCase):
         }"""
         results = conversion_tools.pyalveoQuery(q, limit=True)
 
-        for result in results:
-            print result["var2"]["value"] + ":" + result["text"]["value"]    
+        #for result in results:
+        #    print result["var2"]["value"] + ":" + result["text"]["value"]    
         
 
     def test_followedby(self):
@@ -191,10 +212,11 @@ class TestQueries(unittest.TestCase):
                 ?var1 dada:type maus:phonetic.
                 ?var1 dada:label 'r'.
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         
         hand_short_res = results[0]
-
+        hand_short_ress = results
 
         q="""select ?var0 ?text0
         where {
@@ -208,34 +230,46 @@ class TestQueries(unittest.TestCase):
                         ?var2 dada:label 'r'.
                         ?var0 dada:type maus:phonetic.
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
 
         hand_long_res = results[0]
+        hand_long_ress = results
         
         self.assertEqual(hand_short_res["var0"]["value"], hand_long_res["var0"]["value"])
         self.assertEqual(hand_short_res["text0"]["value"], hand_long_res["text0"]["value"])
+        self.assertEqual(len(hand_short_ress), len(hand_long_ress))
 
         # EmuQL
         query = "[#maus:phonetic!='_'->maus:phonetic='r']"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         emu_res = results[0]
-
+        emu_ress = results
+        
         #print q
 
         self.assertEqual(hand_short_res["var0"]["value"], emu_res["var1"]["value"])
+        self.assertEqual(len(hand_short_ress), len(emu_ress))
         
         # LPath+
         #query = "//_[@dada:type=maus:phonetic]->r[@dada:type=maus:phonetic]"
         query = "//_[->r[@dada:type=maus:phonetic]]"
+        #query = "//_[@dada:type=maus:orthography]/_[->r[@dada:type=maus:phonetic]]"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         lpath_res = results[0]
+        lpath_ress = results
 
         print q
-        print lpath_res
-
+        print len(hand_short_ress)
+        print len(lpath_ress)
+        
         self.assertEqual(hand_short_res["var0"]["value"], lpath_res["var1"]["value"])
+        self.assertEqual(len(hand_short_ress), len(lpath_ress))
+        
 
     def test_not(self):
         """Not: Find words which are not 'time'"""
@@ -247,31 +281,39 @@ class TestQueries(unittest.TestCase):
                 filter not exists {
                     ?var1 dada:label 'time'.}
         }"""
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
 
-        for result in results:
-            print result["text"]["value"] + ":" + result["var1"]["value"]
+        #for result in results:
+        #    print result["text"]["value"] + ":" + result["var1"]["value"]
 
         hand_res = results[0]
+        hand_ress = results
 
         # EmuQL
         query = "maus:orthography!='time'"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         emu_res = results[0]
+        emu_ress = results
         
         self.assertEqual(hand_res["var1"]["value"], emu_res["var1"]["value"])
+        self.assertEqual(len(hand_ress), len(emu_ress))
         
         # LPath+
         query = "//_[not @dada:label='time']"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.pyalveoQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         lpath_res = results[0]
+        lpath_ress = results
 
-        #print q
-        #print lpath_res
+        print q
+        print lpath_res
 
         self.assertEqual(lpath_res["var1"]["value"], emu_res["var1"]["value"])
+        self.assertEqual(len(lpath_ress), len(emu_ress))
 
     def test_empty(self):
         """Empty: Find a word 'time' that has the sound 'r' i.e. should return zero results"""
@@ -324,7 +366,7 @@ class TestQueries(unittest.TestCase):
         where {
                 ?var0 dada:hasChild ?var1.
                 ?var0 dada:hasChild ?var2.
-                ?var1 dada:followedby ?va2.
+                ?var1 dada:followedby ?var2.
                 ?var0 dada:type maus:orthography.
                 ?var0 dada:label 'time'.
                 ?var1 dada:type maus:phonetic.
@@ -332,9 +374,11 @@ class TestQueries(unittest.TestCase):
                 ?var2 dada:type maus:phonetic.
                 ?var2 dada:label ?text2.
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         
         hand_short_res = results[0]
+        hand_short_ress = results
         
 
         # indirect sparql
@@ -361,9 +405,11 @@ class TestQueries(unittest.TestCase):
                 filter (?start1 >= ?start0).
                 filter (?end2 <= ?end0).
         }"""
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
         
         hand_long_res = results[0]
+        hand_long_ress = results
 
         #print hand_short_res["text1"]["value"]
         #print hand_long_res["text1"]["value"]
@@ -376,24 +422,35 @@ class TestQueries(unittest.TestCase):
         self.assertEqual(hand_short_res["text1"]["value"], hand_long_res["text1"]["value"])
         self.assertEqual(hand_short_res["var2"]["value"], hand_long_res["var2"]["value"])
         self.assertEqual(hand_short_res["text2"]["value"], hand_long_res["text2"]["value"])
+        self.assertEqual(len(hand_short_ress), len(hand_long_ress))
 
         # emu
         query = "[maus:orthography='time'^[maus:phonetic!='_'->#maus:phonetic!='_']]"
         q = emu_parser.emuToSparql(query)
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
+
+        print q
 
         emu_res = results[0]
+        emu_ress = results
 
         self.assertEqual(hand_long_res["var2"]["value"], emu_res["var3"]["value"])
+        self.assertEqual(len(hand_long_ress), len(emu_ress))
 
         # lpath
         query = "//time[@dada:type=maus:orthography]/_[@dada:type=maus:phonetic]->_[@dada:type=maus:phonetic]"
         q = lpath_parser.lpathToSparql(query)
-        results = conversion_tools.serverQuery(q, limit=True)
+        #results = conversion_tools.serverQuery(q, limit=True)
+        results = conversion_tools.serverQuery(q)
+
+        print q
 
         lpath_res = results[0]
+        lpath_ress = results
 
         self.assertEqual(hand_long_res["var2"]["value"], lpath_res["var10"]["value"])
+        self.assertEqual(len(hand_long_ress), len(lpath_ress))
 
 
 if __name__ == "__main__":
